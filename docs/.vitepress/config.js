90e19018eca242fa87b34f381fc60fc7d8895d74
panny <coocangela@gmail.com>
2024-01-21 11:31:18 +0800

    add client

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -13,6 +13,16 @@ export default {
     siteTitle: 'Ducky Cloud · 速鸭云',
     nav: [
       {
+        text: '软件下载',
+        items: [
+          { text: 'Mac 电脑', link: '/mac' },
+          { text: '苹果手机平板', link: 'https://wiki.tyro.wiki' },
+          { text: 'Windows 电脑', link: 'https://assets.tyro.wiki/t/okk.exe' },
+          { text: '安卓鸿蒙/手机平板', link: 'https://assets.tyro.wiki/t/okk.apk' },
+          { text: '第三方通用软件', link: 'https://wiki.tyro.wiki' },
+        ]
+      },
+      {
         text: 'Todesk 远程',
         items: [
           { text: 'Todesk 官网下载', link: 'https://www.todesk.com/download.html' },
@@ -21,22 +31,22 @@ export default {
       },
       { text: '账号杂货铺', link: 'http://cloud.idid88.com/' },
     ],
-    sidebar: {
-      // 当用户在 `指南` 目录页面下将会展示这个侧边栏
-      '/down/': [
-        {
-          text: '快速上手',
-          items: [
-            // This shows `/guide/index.md` page.
-            { text: '介绍', link: '/down/' },
-            { text: '苹果 手机&平板', link: '/down/ios' },
-            { text: '安卓与鸿蒙 手机&平板', link: '/down/android' },
-            { text: 'Windows 电脑', link: '/down/windows' },
-            { text: 'Mac 电脑', link: '/down/mac' },
-          ]
-        },
-      ],
-    }
+    // sidebar: {
+    //   // 当用户在 `指南` 目录页面下将会展示这个侧边栏
+    //   '/down/': [
+    //     {
+    //       text: '快速上手',
+    //       items: [
+    //         // This shows `/guide/index.md` page.
+    //         { text: '介绍', link: '/down/' },
+    //         { text: '苹果 手机&平板', link: '/down/ios' },
+    //         { text: '安卓与鸿蒙 手机&平板', link: '/down/android' },
+    //         { text: 'Windows 电脑', link: '/down/windows' },
+    //         { text: 'Mac 电脑', link: '/down/mac' },
+    //       ]
+    //     },
+    //   ],
+    // }
   },
 
 }
