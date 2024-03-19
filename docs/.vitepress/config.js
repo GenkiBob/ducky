3892a1a27714adb9f1a7a30316c319295ea75a6a
panny <coocangela@gmail.com>
2024-03-19 18:24:26 +0800

    windows

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -16,7 +16,7 @@ export default {
         text: '软件下载',
         items: [
           { text: 'Mac 苹果电脑', link: '/mac' },
-          { text: 'Windows 电脑', link: 'https://assets.tyro.wiki/t/okk.exe' },
+          { text: 'Windows 电脑', link: '/windows' },
           { text: 'IOS 苹果手机（平板）', link: 'https://wiki.tyro.wiki' },
           { text: '安卓|鸿蒙 手机（平板）', link: 'https://assets.tyro.wiki/t/okk.apk' },
           { text: '第三方通用软件', link: 'https://wiki.tyro.wiki' },
