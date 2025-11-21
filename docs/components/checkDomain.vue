33aa02d8052c77b6473e2f9572dcbe0a29387d76
panny <coocangela@gmail.com>
2025-11-21 14:06:36 +0800

    update

--- docs/components/checkDomain.vue
+++ docs/components/checkDomain.vue
@@ -1,24 +1,47 @@
 <script setup>
+
+  import { onMounted } from 'vue'
   // 获取当前页面的 URL
 
-  var url = window.location.href;
+  onMounted(() => {
+    var url = window.location.href;
 
-  // 解析 URL，并获取查询参数
-  var urlParams = new URL(url);
-  var queryParams = {};
-  var path = ''; // 初始化 path 为空字符串
-  var timer;
+    // 解析 URL，并获取查询参数
+    var urlParams = new URL(url);
+    var queryParams = {};
+    var path = ''; // 初始化 path 为空字符串
+    var timer;
 
-  // 遍历所有查询参数并放入对象中
-  urlParams.searchParams.forEach(function (value, key) {
-    if (key === "path") {
-      // 如果查询参数名是 "path"，将其赋值给一个变量
-      path = value;
-    } else {
-      // 否则，将其他查询参数放入对象中
-      queryParams[key] = value;
-    }
-  });
+    // 遍历所有查询参数并放入对象中
+    urlParams.searchParams.forEach(function (value, key) {
+      if (key === "path") {
+        // 如果查询参数名是 "path"，将其赋值给一个变量
+        path = value;
+      } else {
+        // 否则，将其他查询参数放入对象中
+        queryParams[key] = value;
+      }
+    });
+
+    fetch("./data.json?t=" + Date.now())
+      .then((res) => res.json())
+      .then((data) => {
+        checkDomain(data); // 立即执行一次
+        timer && clearInterval(timer);
+        // retry every 3 seconds
+        timer = setInterval(() => {
+          checkDomain(data)
+        }, 3000);
+        setTimeout(() => {
+          // 超时 10 秒禁止循环，弹出提示联系站长
+          clearInterval(timer);
+          timer = null;
+          const text = "无法跳转可用站点，请联系客服获取补偿。";
+          alert(text);
+          //   document.getElementById('info').innerText = text;
+        }, 10000)
+      });
+  })
 
   function joinUrlAndPath (url, path) {
     // 移除 URL 和路径末尾的斜杠
@@ -30,25 +53,6 @@
 
     return joinedUrl;
   }
-
-  fetch("./data.json?t=" + Date.now())
-    .then((res) => res.json())
-    .then((data) => {
-      checkDomain(data); // 立即执行一次
-      timer && clearInterval(timer);
-      // retry every 3 seconds
-      timer = setInterval(() => {
-        checkDomain(data)
-      }, 3000);
-      setTimeout(() => {
-        // 超时 10 秒禁止循环，弹出提示联系站长
-        clearInterval(timer);
-        timer = null;
-        const text = "无法跳转可用站点，请联系客服获取补偿。";
-        alert(text);
-        //   document.getElementById('info').innerText = text;
-      }, 10000)
-    });
 
   function checkDomain (hosts) {
     for (var i = 0; i < hosts.length; i++) {
@@ -89,8 +93,8 @@
     <h3>正在检测有效地址</h3>
 
     <div style="margin-top: 30px;">
-      <br>
-      <br>
+      <br />
+      <br />
       <span>
         若5秒内未跳转，请点击
       </span>
